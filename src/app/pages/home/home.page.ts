58e021859a8c6563da1257b14bc760765d530c5e
JuacoNote <joaquinsal.jso@gmail.com>
2025-09-03 12:00:20 -0400

    está todo malo xd

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -4,10 +4,23 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
+  standalone: false
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+
+  //numero:number=0;
+  mensaje:string;
+  //activo:boolean=true;
+
+  constructor() { 
+    this.mensaje="Hola prueba!";
+  }
+  imprimirMsg(){
+    console.log(this.mensaje);
+
+  }
+
 
   ngOnInit() {
   }
